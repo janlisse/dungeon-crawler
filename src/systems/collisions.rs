95edd34c3f2166f1eb3c3da3a302d41a61914a2a
Jan Liße <jan.lisse@posteo.de>
2022-02-19 21:53:01 +0100

    Add collision detection

---
+++ src/systems/collisions.rs
@@ -0,0 +1,21 @@
+use crate::prelude::*;
+use legion::systems::CommandBuffer;
+use legion::world::SubWorld;
+
+#[system]
+#[read_component(Point)]
+#[read_component(Player)]
+#[read_component(Enemy)]
+pub fn collisions(world: &SubWorld, commands: &mut CommandBuffer) {
+    let mut players = <&Point>::query().filter(component::<Player>());
+    let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+
+    let player_pos = players.iter(world).next().expect("Get player");
+
+    enemies
+        .iter(world)
+        .filter(|(_, pos)| **pos == *player_pos)
+        .for_each(|(entity, _)| {
+            commands.remove(*entity);
+        })
+}
